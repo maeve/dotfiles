3f8cce088b66ec3b8ddb039f90d38f95027bbe44
Maeve Revels <maeve@aha.io>
2022-09-27 14:06:59 -0700

    Tweak mappings for telescope

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,7 +1,8 @@
 vim.g.mapleader = ','
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', 'ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
+vim.keymap.set('n', 'ff', builtin.current_buffer_fuzzy_find, {})
 vim.keymap.set('n', 'fg', builtin.live_grep, {})
 vim.keymap.set('n', 'fb', builtin.buffers, {})
 vim.keymap.set('n', 'fh', builtin.help_tags, {})
