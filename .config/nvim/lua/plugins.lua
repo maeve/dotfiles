f96ed3deff588a0cf8b8877353aa234dd9d0544c
Maeve Revels <maeve@aha.io>
2022-10-03 18:51:05 -0700

    Include hidden files in fuzzy find

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -34,14 +34,34 @@ return require('packer').startup(function(use)
       { 'nvim-telescope/telescope-file-browser.nvim' }
     },
     config = function()
-      require('telescope').setup({
+      local telescope = require('telescope')
+      local telescopeConfig = require('telescope.config')
+
+      -- Clone the default Telescope configuration
+      local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+      -- I want to search in hidden/dot files.
+      table.insert(vimgrep_arguments, '--hidden')
+      -- I don't want to search in the `.git` directory.
+      table.insert(vimgrep_arguments, '--glob')
+      table.insert(vimgrep_arguments, '!.git/*')
+
+      telescope.setup({
+        defaults = {
+          vimgrep_arguments = vimgrep_arguments
+        },
         extensions = {
           file_browser = {
             cwd_to_path = true
           }
+        },
+        pickers = {
+          find_files = {
+            find_command = { 'rg', '--files', '--hidden', '--glob', '!.git/*' }
+          }
         }
       })
-      require('telescope').load_extension('file_browser')
+      telescope.load_extension('file_browser')
     end
   }
 
