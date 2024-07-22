bd9c4662467377c9b79b2e587733db5102be89da
Maeve Revels <maeve@aha.io>
2024-07-22 14:23:02 -0700

    More docker aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,9 +1,13 @@
 compdef g="git"
 setopt complete_aliases
 
-alias bgl="SHELL=bash ops breakglass"
-alias om="overmind"
+alias bgl="ops breakglass staging"
+
+
 alias dcrails="docker compose exec web rails"
 alias dcrubocop="docker compose exec web rubocop"
 alias dcrspec="docker compose exec web rspec"
-alias dcbe="docker compose bundle exec"
+alias dcbundle="docker compose exec web bundle"
+alias dcbash="docker compose exec web bash"
+alias dcrake="docker compose exec web rake"
+alias dclogin="ops docker login staging"
