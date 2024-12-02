0ee72333abbcf7e6741db9e9e353711d0dbf0237
Maeve Revels <maeve@aha.io>
2024-12-02 03:19:07 -0800

    Extract rainbow_csv plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -4,27 +4,6 @@ return {
 		cmd = "Mason",
 		opts = {},
 	},
-	{
-		"cameron-wags/rainbow_csv.nvim",
-		config = function()
-			require("rainbow_csv").setup()
-		end,
-		-- optional lazy-loading below
-		module = {
-			"rainbow_csv",
-			"rainbow_csv.fns",
-		},
-		ft = {
-			"csv",
-			"tsv",
-			"csv_semicolon",
-			"csv_whitespace",
-			"csv_pipe",
-			"rfc_csv",
-			"rfc_semicolon",
-		},
-	},
-
 	-- See https://github.com/gbprod/yanky.nvim/issues/75
 	{
 		"gbprod/yanky.nvim",
